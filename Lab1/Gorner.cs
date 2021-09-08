8d6d07ef7be627a40eb0a8e0da5c9f14fb2f8d2e
Vladislav <vsoilol1701@gmail.com>
2021-09-08 21:23:20 +0300

    Created Gorner Algorithm

---
+++ Lab1/Gorner.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Lab1
+{
+    public static class AlgorithmGorner
+    {
+        public static double OrdinaryFunction(int x, double[] k)
+        {
+            double result = 0;
+            for (int i = 0; i < k.Length; i++)
+            {
+                result += k[i] * Math.Pow(x, i);
+            }
+
+            return result;
+        }
+        
+        public static double CalculateWithFor(int x, double[] k)
+        {
+            double result = k[0];
+            
+            for (int i = 1; i < k.Length; i++)
+            {
+                result *= x;
+                result += k[i];
+            }
+
+            return result;
+        }
+        
+        public static double CalculateWithForNegativeStep(int x, double[] k)
+        {
+            double result = k[^1];
+            for (int i = k.Length - 2; i >= 0; i--)
+            {
+                result *= x;
+                result += k[i];
+            }
+
+            return result;
+        }
+        
+        public static double CalculateWithWhile(int x, double[] k)
+        {
+            double result = k[0];
+            int i = 1;
+            
+            while (i < k.Length)
+            {
+                result *= x;
+                result += k[i];
+                i++;
+            }
+
+            return result;
+        }
+        
+        public static double CalculateWithWhileNegativeStep(int x, double[] k)
+        {
+            double result = k[^1];
+            int i = k.Length - 2;
+            
+            while (i >= 0)
+            {
+                result *= x;
+                result += k[i];
+                i--;
+            }
+
+            return result;
+        }
+        
+        public static double CalculateRecursively(double x, double[] a, int i = 0)
+        {
+            if (i >= a.Length)
+                return 0;
+            return a[i] + x * CalculateRecursively(x, a, i + 1);
+        }
+    }
+}
--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 
 namespace Lab1
 {
@@ -6,7 +8,69 @@ namespace Lab1
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            Stopwatch stopwatch = new Stopwatch();
+            double result;
+            int x = -5;
+            double[] k = new double[400]; // В порядке возрастания
+
+            for (int i = 0; i < k.Length; i++)
+            {
+                k[i] = random.Next(-50, 50);
+            }
+
+            double[] reverseK = k.Reverse().ToArray(); // В порядке убывания
+
+            stopwatch.Start();
+            result = AlgorithmGorner.OrdinaryFunction(x, k);
+            stopwatch.Stop();
+
+            Console.WriteLine($"Ordinary result: {result}, time: {stopwatch.Elapsed}");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            result =
+                AlgorithmGorner.CalculateRecursively(x, k);
+            stopwatch.Stop();
+            Console.WriteLine($"Gorner recursively: " +
+                              $"{result} " +
+                              $"time: {stopwatch.Elapsed}");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            result =
+                AlgorithmGorner.CalculateWithFor(x, reverseK);
+            stopwatch.Stop();
+            Console.WriteLine($"Gorner with FOR: " +
+                              $"{result} " +
+                              $"time: {stopwatch.Elapsed}");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            result =
+                AlgorithmGorner.CalculateWithForNegativeStep(x, k);
+            stopwatch.Stop();
+            Console.WriteLine($"Gorner with FOR negative step: " +
+                              $"{result} " +
+                              $"time: {stopwatch.Elapsed}");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            result =
+                AlgorithmGorner.CalculateWithWhile(x, reverseK);
+            stopwatch.Stop();
+            Console.WriteLine($"Gorner with While: " +
+                              $"{result} " +
+                              $"time: {stopwatch.Elapsed}");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            result =
+                AlgorithmGorner.CalculateWithWhileNegativeStep(x, k);
+            stopwatch.Stop();
+            Console.WriteLine($"Gorner with While negative step: " +
+                              $"{result} " +
+                              $"time: {stopwatch.Elapsed}");
         }
     }
 }
