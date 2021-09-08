f914234f57dd2640c3702dc451c48e2572e55329
Vladislav <vsoilol1701@gmail.com>
2021-09-08 20:08:13 +0300

    Create project

---
+++ Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
