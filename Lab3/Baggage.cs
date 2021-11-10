ce48a0e65747f103813cc2d99f5f4e3d786b750d
Vladislav_Sokolov <vsoilol1701@gmail.com>
2021-11-11 00:03:41 +0300

    fixed

---
+++ Lab3/Baggage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Lab3.Extensions;
+
+namespace Lab3
+{
+    public struct Baggage
+    {
+        public int ThingsAmount { get; set; }
+
+        public double ThingsWeight { get; set; }
+
+        public override string ToString()
+        {
+            return $"Количество вещей: {ThingsAmount}\n" +
+                   $"Общий вес вещей: {ThingsWeight}\n";
+        }
+    }
+}
